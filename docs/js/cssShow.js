f7688ba5118e1b502b5ed826462a4e4873c90cfa
jsndyks <j.dykes@city.ac.uk>
2026-09-23 13:44:01 +0100

    updates css SHow

--- docs/js/cssShow.js
+++ docs/js/cssShow.js
@@ -1,3 +1,17 @@
-  if (new URLSearchParams(location.search).get("css") === "show") {
-    document.documentElement.classList.add("show-css-info");
-  }
+if (new URLSearchParams(location.search).get("css") === "show") {
+
+  const stylesheets = [...document.querySelectorAll('link[rel="stylesheet"]')];
+
+  const names = stylesheets
+    .map(link => {
+      try {
+        return new URL(link.href).pathname.split('/').pop();
+      } catch {
+        return link.href;
+      }
+    })
+    .filter(Boolean);
+
+  document.documentElement.classList.add("show-css-info");
+  document.documentElement.dataset.cssInfo = names.join(" · ");
+}
