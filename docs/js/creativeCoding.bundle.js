f6445c87badf10544fac98dcf26fc080ec7c5d72
jsndyks <j.dykes@city.ac.uk>
2026-09-25 10:29:42 +0100

    updates js bundle!

--- docs/js/creativeCoding.bundle.js
+++ docs/js/creativeCoding.bundle.js
@@ -133,41 +133,6 @@ eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a
 
 
 /* ==========================================================================
-   Highslide Site Configuration
-   Source: highslide-cfg.js
-   ========================================================================== */
-
-/* Site-wide configuration of the highslide window appearance */
-/* Jo Wood, 10th September, 2014 */
-hs.graphicsDir = 'http://staff.city.ac.uk/~jwo/moodle/css/graphics/';
-hs.showCredits = false;
-hs.outlineType = 'rounded-white'; 
-hs.wrapperClassName = 'draggable-header'; 
-hs.preserveContent = false;
-hs.enableKeyListener = false;
-
-
-
-
-
-function loadSketch(sketchSrc, sketchWidth, imgWidth, imgSrc)
-{
-    document.write("<div><a href=\""+sketchSrc+"/index.html\" onclick=\"return hs.htmlExpand(this, { objectType: 'iframe', minWidth: "+sketchWidth+"})\">");
-    document.write("<img src=\""+imgSrc+"\" width=\""+imgWidth+"\" alt=\"click to see sketch\" /></a></div>");
-}
-
-
-function loadImage(imgFile, imgWidth, imgHeight, imgAlt)
-{
-
-    document.write("<div><a href=\""+imgFile+"\" onclick=\"return hs.htmlExpand(this, { objectType: 'iframe', minWidth: "+imgWidth+"})\">");
-    document.write("<img src=\""+imgFile+"\" width=\""+imgWidth+"\"  height=\""+imgHeight+"\" alt=\""+imgAlt+"\" /></a></div>");
-    
-}
-;
-
-
-/* ==========================================================================
    Site Master JavaScript
    Source: master.js
    ========================================================================== */
@@ -211,3 +176,86 @@ $(function()
 	});
 });
 ;
+
+/*
+ * Site-wide configuration of the Highslide window appearance
+ * Original: Jo Wood, 10th September 2014
+ *
+ * Updated: 25th September 2026
+ * - Highslide graphics moved to:
+ *   https://jsndyks.github.io/cc26/js/city/graphics/
+ * - loadSketch() now sets iframe objectWidth and objectHeight.
+ * - Existing four-argument loadSketch() calls remain compatible:
+ *   sketchHeight defaults to sketchWidth.
+ * - loadImage() now sets iframe objectWidth and objectHeight explicitly.
+ */
+
+hs.graphicsDir = 'https://jsndyks.github.io/cc26/js/city/graphics/';
+hs.showCredits = false;
+hs.outlineType = 'rounded-white';
+hs.wrapperClassName = 'draggable-header';
+hs.preserveContent = false;
+hs.enableKeyListener = false;
+
+
+/**
+ * Load an interactive sketch in a Highslide iframe.
+ *
+ * sketchSrc    URL of sketch folder, without /index.html
+ * sketchWidth  width of popup iframe
+ * imgWidth     width of preview image
+ * imgSrc       URL of preview image
+ * sketchHeight optional popup height; defaults to sketchWidth
+ */
+function loadSketch(sketchSrc, sketchWidth, imgWidth, imgSrc, sketchHeight)
+{
+    // Backward compatibility:
+    // old four-argument calls use a square popup.
+    if (sketchHeight === undefined) {
+        sketchHeight = sketchWidth;
+    }
+
+    document.write(
+        "<div><a href=\"" + sketchSrc + "/index.html\" " +
+        "onclick=\"return hs.htmlExpand(this, {" +
+        "objectType: 'iframe', " +
+        "objectWidth: " + sketchWidth + ", " +
+        "objectHeight: " + sketchHeight +
+        "})\">"
+    );
+
+    document.write(
+        "<img src=\"" + imgSrc +
+        "\" width=\"" + imgWidth +
+        "\" alt=\"click to see sketch\" /></a></div>"
+    );
+}
+
+
+/**
+ * Load an image in a Highslide iframe.
+ *
+ * imgFile   URL of image
+ * imgWidth  width of popup/image
+ * imgHeight height of popup/image
+ * imgAlt    alternative text
+ */
+function loadImage(imgFile, imgWidth, imgHeight, imgAlt)
+{
+    document.write(
+        "<div><a href=\"" + imgFile + "\" " +
+        "onclick=\"return hs.htmlExpand(this, {" +
+        "objectType: 'iframe', " +
+        "objectWidth: " + imgWidth + ", " +
+        "objectHeight: " + imgHeight +
+        "})\">"
+    );
+
+    document.write(
+        "<img src=\"" + imgFile +
+        "\" width=\"" + imgWidth +
+        "\" height=\"" + imgHeight +
+        "\" alt=\"" + imgAlt +
+        "\" /></a></div>"
+    );
+}
